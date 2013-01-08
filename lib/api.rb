819f3f9e894d3839dc20a590eebcf35c3f40413d
Chris Couzens <chris.couzens@in4systems.com>
2013-01-08 17:34:06 +0000

    Start the basic classes

---
+++ lib/api.rb
@@ -0,0 +1,4 @@
+class SQLAnywhere::API
+  def self.sqlany_initialize_interface( api )
+  end
+end
---
+++ lib/sqlanywhere.rb
@@ -0,0 +1,6 @@
+class SQLAnywhere
+end
+
+
+require File.dirname(__FILE__) + '/sql_anywhere_interface.rb'
+require File.dirname(__FILE__) + '/api.rb'
