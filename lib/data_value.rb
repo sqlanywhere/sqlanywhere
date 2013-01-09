60d0b8cd34d21027a7dbf73697abff2e8194017d
Chris Couzens <chris.couzens@in4systems.com>
2013-01-09 16:00:32 +0000

    Fully implement sqlany_get_column

--- lib/data_value.rb
+++ lib/data_value.rb
@@ -28,23 +28,23 @@ class SQLAnywhere::DataValue < FFI::Struct
     when :string
       self[:buffer].get_string(0, length)
     when :double
-      self[:value].read_double
+      self[:buffer].read_double
     when :val64 # 64 bit integer
-      self[:buffer].read_int
+      self[:buffer].read_long
     when :uval64 # 64 bit unsigned integer
-      self[:buffer].read_uint
+      self[:buffer].read_ulong
     when :val32
       self[:buffer].read_int
     when :uval32
       self[:buffer].read_uint
     when :val16
-      self[:buffer].get_int
+      self[:buffer].read_short
     when :uval16
-      self[:buffer].read_uint
+      self[:buffer].read_ushort
     when :val8
-      self[:buffer].read_int
+      self[:buffer].read_char
     when :uval8
-      self[:buffer].read_uint
+      self[:buffer].read_uchar
     else
       raise "Type not yet implemented #{self[:type]}"
     end
