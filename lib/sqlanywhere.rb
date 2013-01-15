9adcc28c088c151b27bbb3e590d28530d6d6eacd
Chris Couzens <chris.couzens@in4systems.com>
2013-01-15 17:13:58 +0000

    Add all values for the enumerations

--- lib/sqlanywhere.rb
+++ lib/sqlanywhere.rb
@@ -7,26 +7,54 @@ class SQLAnywhere
 
   extend FFI::Library
 
+  # http://dcx.sybase.com/1200/en/dbprogramming/programming-sacpp-sacapi-h-fil-a-sqlany-data-direction-enu.html
+  DataDirection = enum [
+    :invalid, 0,
+    :input,
+    :output,
+    :input_output,
+  ]
+
+  # http://dcx.sybase.com/1200/en/dbprogramming/programming-sacpp-sacapi-h-fil-a-sqlany-data-type-enu.html
   DataType = enum [
     :invalid_type,
     :binary,
     :string,
     :double,
     :val64, # 64 bit integer
-    :uval64, # 64 bit unsigned integer
+    :unsigned_val64, # 64 bit unsigned integer
     :val32,
-    :uval32,
+    :unsigned_val32,
     :val16,
-    :uval16,
+    :unsigned_val16,
     :val8,
-    :uval8,
+    :unsigned_val8,
   ]
 
-  DataDirection = enum [
-    :invalid, 0,
-    :input,
-    :output,
-    :input_output,
+  # http://dcx.sybase.com/1200/en/dbprogramming/programming-sacpp-sacapi-h-fil-a-sqlany-native-type-enu.html
+  NativeType = enum [
+    :no_type,
+    :date,
+    :time,
+    :timestamp,
+    :var_char,
+    :fix_char,
+    :long_var_char,
+    :string,
+    :double,
+    :float,
+    :decimal,
+    :int,
+    :small_int,
+    :binary,
+    :long_binary,
+    :tiny_int,
+    :big_int,
+    :unsigned_int,
+    :unsigned_small_int,
+    :unsigned_big_int,
+    :bit,
+    :long_n_varchar,
   ]
 
 end
