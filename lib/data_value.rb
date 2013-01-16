a25b64b3121f138613ac64f0f8a423509fc3e988
Chris Couzens <chris.couzens@in4systems.com>
2013-01-16 10:00:50 +0000

    Fix the data values understanding of types
    Passes a couple tests again.
    
    The segfault was avoided just by turning off most of the tests

--- lib/data_value.rb
+++ lib/data_value.rb
@@ -33,19 +33,19 @@ class SQLAnywhere::DataValue < FFI::Struct
       self[:buffer].read_double
     when :val64 # 64 bit integer
       self[:buffer].read_long
-    when :uval64 # 64 bit unsigned integer
+    when :unsigned_val64 # 64 bit unsigned integer
       self[:buffer].read_ulong
     when :val32
       self[:buffer].read_int
-    when :uval32
+    when :unsigned_val32
       self[:buffer].read_uint
     when :val16
       self[:buffer].read_short
-    when :uval16
+    when :unsigned_val16
       self[:buffer].read_ushort
     when :val8
       self[:buffer].read_char
-    when :uval8
+    when :unsigned_val8
       self[:buffer].read_uchar
     else
       raise "Type not yet implemented #{self[:type]}"
